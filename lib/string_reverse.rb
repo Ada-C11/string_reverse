b7c4e9a2399dc22c0660279a242f45b4e537e350
Elise Pham <elise.pham88@gmail.com>
2019-03-17 20:20:09 -0700

    completed string_reserve algorithm

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,6 +1,20 @@
 # A method to reverse a string in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(1)
 def string_reverse(my_string)
-  raise NotImplementedError
+  if my_string.nil?
+    return nil
+  end
+
+  low = 0
+  high = my_string.length - 1
+
+  while low < high
+    temp = my_string[low]
+    my_string[low] = my_string[high]
+    my_string[high] = temp
+    low += 1
+    high -= 1
+  end
+  return my_string
 end
