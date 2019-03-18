efd7e71fac2c66b3ab285b3cd7bee2e682e7b1f6
Carla Bosco <carlabosco@gmail.com>
2019-03-17 18:22:19 -0700

    Completed method with time and space complexities

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,6 +1,20 @@
 # A method to reverse a string in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), where n is length of array
+# Space complexity: O(1)
 def string_reverse(my_string)
-  raise NotImplementedError
+  if my_string == nil
+    return my_string
+  else
+    f = 0
+    l = my_string.length - 1
+
+    while f < l
+      temp = my_string[f]
+      my_string[f] = my_string[l]
+      my_string[l] = temp
+      f += 1
+      l -= 1
+    end
+    return my_string
+  end  
 end
