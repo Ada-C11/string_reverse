3a7bcdd6023aa3419bcb4e3d847201df85120292
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-03-18 20:39:27 -0700

    completed method for string reversal in place

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,6 +1,19 @@
 # A method to reverse a string in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where in is the number of letters in the word
+# Space complexity: O(1)
 def string_reverse(my_string)
-  raise NotImplementedError
+  if my_string != nil
+    i = 0
+    last = my_string.length - 1
+    temp = 0
+
+    while i < last
+      temp = my_string[i]
+      my_string[i] = my_string[last]
+      my_string[last] = temp
+      i += 1
+      last -= 1
+    end
+  end
+  return my_string
 end
