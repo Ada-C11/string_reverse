08569b280ae7e9e8a5209c0983a6289703746185
laneia <laneiamurray@gmail.com>
2019-03-20 14:31:33 -0700

    all tests passing

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,6 +1,17 @@
 # A method to reverse a string in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(1)
+
 def string_reverse(my_string)
-  raise NotImplementedError
+  return nil if my_string == nil
+
+  length = my_string.length
+
+  (length / 2).times do |i|
+    temp_i = my_string[my_string.length - (i + 1)]
+    my_string[my_string.length - (i + 1)] = my_string[i]
+    my_string[i] = temp_i
+  end
+
+  return my_string
 end
