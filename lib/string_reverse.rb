7529611efc79cb2fe7a07a048af9b527ab42cc21
Erica Norris <norrise120@gmail.com>
2019-03-20 20:52:47 -0700

    Added string_reverse method to pass tests

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -2,5 +2,15 @@
 # Time complexity: ?
 # Space complexity: ?
 def string_reverse(my_string)
-  raise NotImplementedError
+  return nil if my_string == nil
+  i = 0
+  j = my_string.length - 1
+  while i < j
+    temp = my_string[i]
+    my_string[i] = my_string[j]
+    my_string[j] = temp
+    i += 1
+    j -= 1
+  end
+  return my_string
 end
