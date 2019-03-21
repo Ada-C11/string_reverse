16196fa68ab2715ac7779f69df070b184c153d4e
Amy Wyatt <amy.wyatt12@outlook.com>
2019-03-20 18:17:57 -0700

    implemented string reverse

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -2,5 +2,21 @@
 # Time complexity: ?
 # Space complexity: ?
 def string_reverse(my_string)
-  raise NotImplementedError
+  if my_string == nil
+    return nil
+  end
+
+  length = my_string.length
+  i = 0
+  j = length - 1
+
+  while i < j
+    character_place = my_string[j]
+    my_string[j] = my_string[i]
+    my_string[i] = character_place
+    i += 1
+    j -= 1
+  end
+
+  return my_string
 end
