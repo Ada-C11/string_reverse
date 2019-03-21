0132cf28400674f598c35ba306aa0d12d6775564
Mina Shin <minamishin@hotmail.com>
2019-03-21 00:26:07 -0700

    array reverse

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,6 +1,17 @@
 # A method to reverse a string in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(1)
 def string_reverse(my_string)
-  # raise NotImplementedError
+  return nil if my_string.nil?
+  n = my_string.length
+  i = 0
+  j = n - i - 1
+
+  while i < j
+    word = my_string[i]
+    my_string[i] = my_string[j]
+    my_string[j] = word
+    i = i + 1
+    j = j - 1
+  end
 end
