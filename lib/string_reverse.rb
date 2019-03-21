23c4b59505539967e6511c57e74508b9d4a13657
Stephanie Marchante <smarcha04@gmail.com>
2019-03-20 20:18:43 -0700

    created string reverse method

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -2,5 +2,18 @@
 # Time complexity: ?
 # Space complexity: ?
 def string_reverse(my_string)
-  raise NotImplementedError
+  if my_string.nil?
+    return nil
+  end
+  i = 0
+  j = my_string.length - 1
+  while i < j
+    b = my_string[j]
+    a = my_string[i]
+    my_string[i] = b
+    my_string[j] = a
+    j -= 1
+    i += 1
+  end
 end
+
