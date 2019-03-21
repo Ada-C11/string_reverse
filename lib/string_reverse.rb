bee46fe07a49d34e2578b594bb0968e12746fffd
Angela <angela.ohh@gmail.com>
2019-03-20 17:45:54 -0700

    added functionality to reverse string

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,6 +1,20 @@
 # A method to reverse a string in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n is number of values in my_string
+# Space complexity: O(n) where n is the size of input
 def string_reverse(my_string)
-  raise NotImplementedError
+  if my_string == nil or my_string == ""
+    return nil
+  else
+    smaller_index = 0
+    larger_index = (my_string.length - 1)
+
+    until smaller_index > larger_index
+      temp = my_string[smaller_index]
+      my_string[smaller_index] = my_string[larger_index]
+      my_string[larger_index] = temp
+      smaller_index += 1
+      larger_index -= 1
+    end
+    return my_string
+  end
 end
