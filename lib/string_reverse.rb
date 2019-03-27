785515391e7d02d72a2aa5e4ad28e1c3ba0347cc
Bitaman <b.amani.s@gmail.com>
2019-03-27 10:06:40 -0700

    passes all the test

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -2,5 +2,20 @@
 # Time complexity: ?
 # Space complexity: ?
 def string_reverse(my_string)
-  raise NotImplementedError
+  if my_string == nil 
+    return nil
+  elsif my_string != ""
+  k = my_string.length / 2
+  i = 0
+  j = -1
+  temp = 0
+  k.times do
+    temp = my_string[i]
+    my_string[i] = my_string[j]
+    my_string[j] = temp
+    i += 1
+    j -= 1
+  end
+  end
+  return my_string  
 end
