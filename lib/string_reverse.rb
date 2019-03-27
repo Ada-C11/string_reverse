8ee68cebe69355510c31c32d332203456120fc89
Bitaman <b.amani.s@gmail.com>
2019-03-27 10:15:01 -0700

    added space and time complexity

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,6 +1,6 @@
 # A method to reverse a string in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: Constant O(1) as we need three spaces for i, j and k ant that never changes
+# Space complexity: linear O(n) where n is the number of elements in the string
 def string_reverse(my_string)
   if my_string == nil 
     return nil
@@ -8,7 +8,7 @@ def string_reverse(my_string)
   k = my_string.length / 2
   i = 0
   j = -1
-  temp = 0
+  temp = ""
   k.times do
     temp = my_string[i]
     my_string[i] = my_string[j]
